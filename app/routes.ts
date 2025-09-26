3cbeee71bb42b3737f258df7a9b10d52ddb46581
Facelles <122049357+Facelles@users.noreply.github.com>
2025-09-26 15:06:02 +0300

    add routing for admin

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,13 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
+import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 
 const routes: RouteConfig = [
-  route("dashboard", "./routes/admin/dashboard.tsx"),
+  layout("./routes/admin/admin-layout.tsx", [
+    route("dashboard", "./routes/admin/dashboard.tsx"),
+    route("all-users", "./routes/admin/all-users.tsx"),
+  ]),
+  route("/", "./routes/main.tsx"),
+
 ];
 
 export default routes;
---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Allusers = () => {
+  return (
+    <div>
+        Users Table
+    </div>
+  )
+}
+
+export default Allusers

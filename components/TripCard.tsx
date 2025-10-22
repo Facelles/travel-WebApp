fe52b6b162d08ab9ec4012c2bb39ef6ca0a9c6f5
Facelles <122049357+Facelles@users.noreply.github.com>
2025-10-22 13:54:54 +0300

    Admin Dashboard ly UI

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,55 +1,73 @@
 import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router"
+import { Link, useLocation } from "react-router";
 import { getFirstWord, cn } from "~/lib/utils";
 
-const TripCard = ({ id, name, location, imageUrl, tags, price }: TripCardProps) => {
+const TripCard = ({
+  id,
+  name,
+  location,
+  imageUrl,
+  tags,
+  price,
+}: TripCardProps) => {
   const [ChipListComponent, setChipListComponent] = useState<any>(null);
-  const [ChipsDirective, setChipsDirective] = useState<any>(null)
+  const [ChipsDirective, setChipsDirective] = useState<any>(null);
+  const [ChipDirective, setChipDirective] = useState<any>(null);
   const path = useLocation();
 
   useEffect(() => {
     import("@syncfusion/ej2-react-buttons").then((pkg) => {
       setChipListComponent(() => pkg.ChipListComponent);
       setChipsDirective(() => pkg.ChipsDirective);
-    })
-  }, [])
+      setChipDirective(() => pkg.ChipDirective);
+    });
+  }, []);
 
-    if (!ChipListComponent || !ChipsDirective) return null;
+  if (!ChipListComponent || !ChipsDirective || !ChipDirective) return null;
 
   return (
-    <Link to={path.pathname === '/' || path.pathname.startsWith('/travel') ? `/travel/${id}` : `/trips/${id}`}
-     className="trip-card">
+    <Link
+      to={
+        path.pathname === "/" || path.pathname.startsWith("/travel")
+          ? `/travel/${id}`
+          : `/trips/${id}`
+      }
+      className="trip-card"
+    >
       <img src={imageUrl} alt={name} />
 
       <article>
         <h2>{name}</h2>
         <figure>
-          <img src="/assets/icons/location-mark.svg" alt="location-mark"
-          className="size-4"
-           />
+          <img
+            src="/assets/icons/location-mark.svg"
+            alt="location-mark"
+            className="size-4"
+          />
+          <figcaption>{location}</figcaption>
         </figure>
-        <figcaption>{location}</figcaption>
       </article>
 
       <div className="mt-5 pl-[18px] pr-3.5 pb-5">
         <ChipListComponent id="travel-chip">
           <ChipsDirective>
             {tags.map((tag, index) => (
-              <ChipsDirective>
+              <ChipDirective
                 key={index}
                 text={getFirstWord(tag)}
-                cssClass={cn(index === 1
-                  ? '!bg-pink-50 !text-pink-500'
-                  : '!bg-success-50 !text-success-700'
+                cssClass={cn(
+                  index === 1
+                    ? "!bg-pink-50 !text-pink-500"
+                    : "!bg-success-50 !text-success-700"
                 )}
-              </ChipsDirective>
+              />
             ))}
           </ChipsDirective>
         </ChipListComponent>
       </div>
       <article className="tripCard-pill">{price}</article>
     </Link>
-  )
-}
+  );
+};
 
-export default TripCard
+export default TripCard;
